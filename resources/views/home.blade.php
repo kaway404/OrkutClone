cab979a4b22628a053dcba236ed84fb6e8f3a31a
Alexandre Silva <xande1231231@hotmail.com>
2018-06-10 16:15:13 -0300

    Fix layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -36,6 +36,48 @@
 
     <div class="panelstatus">
 
+
+    <div class="uk-position-relative uk-visible-toggle uk-light news" uk-slider>
+
+    <p style="font-size: 18px; color: #1e87f0;">Amigos sugeridos</p>
+
+    <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-4@m">
+        <li>
+            <img src="/img/avatar/{{ Auth::user()->photo }}" alt="">
+            <div class="uk-position-center uk-panel"><h1>1</h1></div>
+        </li>
+        <li>
+            <img src="/img/avatar/{{ Auth::user()->photo }}" alt="">
+            <div class="uk-position-center uk-panel"><h1>2</h1></div>
+        </li>
+        <li>
+            <img src="/img/avatar/{{ Auth::user()->photo }}" alt="">
+            <div class="uk-position-center uk-panel"><h1>3</h1></div>
+        </li>
+        <li>
+            <img src="/img/avatar/{{ Auth::user()->photo }}" alt="">
+            <div class="uk-position-center uk-panel"><h1>4</h1></div>
+        </li>
+        <li>
+            <img src="/img/avatar/{{ Auth::user()->photo }}" alt="">
+            <div class="uk-position-center uk-panel"><h1>5</h1></div>
+        </li>
+        <li>
+            <img src="/img/avatar/{{ Auth::user()->photo }}" alt="">
+            <div class="uk-position-center uk-panel"><h1>4</h1></div>
+        </li>
+        <li>
+            <img src="/img/avatar/{{ Auth::user()->photo }}" alt="">
+            <div class="uk-position-center uk-panel"><h1>4</h1></div>
+        </li>
+    </ul>
+
+    <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+    <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
+
+</div>
+
+
     </div>
 
 
